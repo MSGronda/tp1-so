259bf8eb986d89bd0cb221273ef47d82c41b420e
MSGronda <mgronda@itba.edu.ar>
2022-09-03 16:23:24 -0300

    Forks y pipes funcionando.

--- md5.c
+++ md5.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define NUM_SLAVES 5
 
@@ -13,7 +14,7 @@ int main(int argc, char * argv[]){
 	int parent_to_child[num_slaves][2];
 	int child_to_parent[num_slaves][2];
 
-	// creamos los pipes
+	// #### creamos los pipes #### 
 	for(int i=0; i<num_slaves; i++){
 		// fd[0] = read  | fd[1] = write
 		if(pipe(parent_to_child[i]) == -1 || pipe(child_to_parent[i]) == -1){
@@ -22,7 +23,7 @@ int main(int argc, char * argv[]){
 		}
 	}
 
-	// creamos los esclavos
+	// #### creamos los esclavos #### 
 	int i, currentId=1;
 	for(i=0; i<num_slaves && currentId!=0 ;i++){
 		currentId = fork();
@@ -35,20 +36,35 @@ int main(int argc, char * argv[]){
 
 		pids[i] = currentId;
 	}
+
+	// #### TODO: hacerlo mas lindo #### 
 	i--;
 
-	// proceso hijo
+
+	//  #### proceso hijo #### 
 	if(currentId == 0){
+		// cerramos los pipes que no vamos a usar
 		close(parent_to_child[i][1]);		// no quiere escribir
 		close(child_to_parent[i][0]);		// no quiere leer
+
+		
 	}
-	// proceso padre
+
+
+	//  #### proceso padre #### 
 	else{
 		// cerramos los pipes que no vamos a usar
 		for(int j=0; j<num_slaves; j++){
 			close(parent_to_child[j][0]);		// no quiere leer 
 			close(child_to_parent[j][1]);		// no quiere escribir
+
 		}
-		printf("soy padre!\n");
+
+
+		// #### esperamos a todos los hijos #### 
+		for(int j=0; j<num_slaves; ){
+			if(wait(NULL)>0)
+				j++;
+		}
 	}
 }
