fd0a459897624b0d15fa17685923948efb7d92b2
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-09-06 20:40:17 -0300

    Further refactoring done.

--- md5.c
+++ md5.c
@@ -1,8 +1,8 @@
 #define _POSIX_SOURCE
 
 #include <math.h>
-#include <stdio.h>
 #include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/select.h>
 #include <sys/types.h>
@@ -36,171 +36,162 @@ main(int argc, char * argv[])
 	fd_set read_fd, backup_read_fd;
 	FD_ZERO(&read_fd);
 
-	// #### creamos los pipes #### 
+	// Create pipes
 	for(int i=0; i < num_slaves; i++){
 		if(pipe(slaves[i].app_to_slave) == -1 || pipe(slaves[i].slave_to_app) == -1) {
-			perror("ERROR! Pipes!\n");
-			exit(2);
+			perror("Creating initial pipes");
+			exit(1);
 		}
 
 		FD_SET(slaves[i].slave_to_app[READ], &read_fd);	
 	}
 	backup_read_fd = read_fd;
 
-	// #### creamos los esclavos #### 
-	int curr_slave, currentId = 1;
-	for(curr_slave = 0; curr_slave < num_slaves && currentId != 0 ; curr_slave++) {
-		// cheque de error
-		if((currentId = fork()) == -1){
-			perror("ERROR! Fork!\n");
-			exit(1);
+	// Create slaves
+	int curr_slave, curr_id = 1;
+	for(curr_slave = 0; curr_slave < num_slaves && curr_id != 0 ; curr_slave++) {
+		if((curr_id = fork()) == -1){
+			perror("Forking slaves");
+			exit(2);
 		}
 
-		slaves[curr_slave].pid = currentId;
+		slaves[curr_slave].pid = curr_id;
 	}
-
 	// #### TODO: hacerlo mas lindo #### 
 	curr_slave--;
 
 
-	//  #### proceso esclavo #### 
-	if(currentId == 0){
-		// cerramos los pipes que no vamos a usar
-		close(slaves[curr_slave].app_to_slave[WRITE]);
-		close(slaves[curr_slave].slave_to_app[READ]);
+	/* SLAVE */
+	if(curr_id == 0)
+        {
+		// Close useless pipes
+		if(close(slaves[curr_slave].app_to_slave[WRITE]) == -1 || close(slaves[curr_slave].slave_to_app[READ]) == -1) {
+                    perror("Closing useless pipes in slave");
+                    exit(3);
+                }
 
-		// variables
-		char * file;
 		int fd[2];
-		char * arg[] = {"md5sum", NULL, NULL};	// el segundo elemento va a ser el path que recibimos del padre
+		char * args[] = { "md5sum", NULL, NULL };	// 2nd arg will be path received from APP
 
-		// creamos los pipes para comunicar subesclavo con esclavo
-		if(pipe(fd)==-1){
-			perror("ERROR! Pipe de slave!\n");
-			exit(3);
+                // Create pipe
+		if(pipe(fd) == -1) {
+			perror("Creating pipe in slave");
+			exit(4);
 		}
 
 		int original = dup(1);
-		// redireccionamos la entrada y salida estandard al pipe
-		dup2(fd[1], 1);	// TODO: chequear error
-		dup2(fd[0],0);
+		// Redirect IO to pipe
+                if(dup2(fd[READ],0) == -1 || dup2(fd[WRITE], 1) == -1) {
+                    perror("Rediecting IO pipe to slave");
+                    exit(5);
+                }
 		
 		// TODO: cerrar todos los fd que no usamos
+                
+		int finished = 0;
+		char * file;
+		char output[MD5_SIZE + 1]= {0};
 
-		int finished=0;
-		char salida[MD5_SIZE + 1]= {0};
-
-		while(finished==0){
-			
-			// leemos lo que le mando
+		while(finished==0) {
+                        // Read what app sent
 			while(read(slaves[curr_slave].app_to_slave[READ], &file, sizeof(char *)) < 1);			
 			//TODO: encontrar forma de hacer esto correctamente!!!!!!!!
 
-			// #### creamos sub esclavo que va a correr el md5sum ####
-			int id = fork();
-			if(id==-1){
-				perror("ERROR! Pipes en esclavo!\n");
-				exit(4);
+			int id;
+			if((id = fork()) == -1){
+				perror("Forking subslave");
+				exit(6);
 			}
 
-			// #### proceso subesclavo #### 
-			if(id==0){
-				arg[1] = file;			//le pasamos como argumento el nombre del file
-				execvp("md5sum", arg);
+                        /* SUBSLAVE: Only job is to execute md5sum */
+			if(id == 0) {
+				args[1] = file;	// arg will be filename		
+				execvp("md5sum", args);
 			}
+                        /* SLAVE */
+			else {				
+				while(read(0, output, MD5_SIZE * sizeof(char)) < 1);		// TODO: esto se puede solucionar con semaforos o estamos forzados a hacerlo asi (en este caso)???
 
+				// Flush stdin
+				int dump;
+				while ((dump = getchar()) != '\n' && dump != EOF);
 
-			// #### proceso esclavo #### 
-			else{				
-
-				while(read(0, salida, MD5_SIZE * sizeof(char)) < 1);		// TODO: esto se puede solucionar con semaforos o estamos forzados a hacerlo asi (en este caso)???
-
-				// flusheamos la entrada estandar 
-				int c;
-				while ((c = getchar()) != '\n' && c != EOF);
-
-				// lo esperamos al subesclavo 
-				wait(NULL);
+				wait(NULL); // Wait for subslave
 
  				dup2(original, 1);
- 				printf("\tHijo i=%d manda: %s -> %s\n\n",curr_slave, file,salida);
+ 				printf("\tHijo i=%d manda: %s -> %s\n\n",curr_slave, file,output);
  				dup2(fd[1],1);
 
- 				write(slaves[curr_slave].slave_to_app[WRITE], salida, MD5_SIZE*sizeof(char));
+ 				write(slaves[curr_slave].slave_to_app[WRITE], output, MD5_SIZE*sizeof(char));
   			}		
 		}
 	}
+        /* APP */
+	else
+        {
+		char ans[MD5_SIZE + 1]= {0};
+		int curr_files_sent = 0, curr_files_read = 0;	// Ignore first file bc it is the executable's name
 
 
-	//  #### proceso padre #### 
-	else{
-		char respuesta[MD5_SIZE + 1]= {0};
-		int current_file_sent = 0, current_file_read = 0;				// ignoramos el primer file que es el nombre del ejecutable
-
-
-		// cerramos los pipes que no vamos a usar
-		for(int j=0; j<num_slaves; j++){
-			close(slaves[j].app_to_slave[READ]);		// no quiere leer 
-			close(slaves[j].slave_to_app[WRITE]);		// no quiere escribir
+		// Close useless pipes
+		for(int i = 0; i < num_slaves; i++) {
+                    if(close(slaves[i].app_to_slave[READ]) == -1 || close(slaves[i].slave_to_app[WRITE]) == -1) {
+                        perror("Closing useless pipes in app");
+                        exit(3);
+                    }
 		}
 
-		// Le pasamos inicialmente a todos los esclavos
-		for(int i=0 ; current_file_sent<num_slaves; i++, current_file_sent++){
-			write(slaves[i].app_to_slave[WRITE], &(argv[current_file_sent]), sizeof(char *));
-		}
+		// Initial distribution of files to slaves
+		for(int i = 0; curr_files_sent < num_slaves; i++, curr_files_sent++)
+			write(slaves[i].app_to_slave[WRITE], &(argv[curr_files_sent]), sizeof(char *));
 
-		// recibimos y mandamos files hasta que no queden mas para procesar
-		while(current_file_read < num_files){
-
-			if(select(FD_SETSIZE, &read_fd, NULL, NULL, NULL)<0){
-				perror("Select");
-				exit(5);
+		// Send files until there are no more to process
+		while(curr_files_read < num_files) {
+			if(select(FD_SETSIZE, &read_fd, NULL, NULL, NULL) < 0) {
+				perror("Select in app");
+				exit(7);
 			}
 
-			// 
-			for(int i=0; i<num_slaves && current_file_read < num_files ; i++){
-
-				if(FD_ISSET(slaves[i].slave_to_app[READ], &read_fd)){
-
-					int read_val = read(slaves[i].slave_to_app[READ], respuesta, MD5_SIZE*sizeof(char));
-					current_file_read++;
+			for(int i = 0; i < num_slaves && curr_files_read < num_files; i++) {
+				if(FD_ISSET(slaves[i].slave_to_app[READ], &read_fd)) {
+					int read_val = read(slaves[i].slave_to_app[READ], ans, MD5_SIZE*sizeof(char));
+					curr_files_read++;
 
 					if(read_val == -1){
-						perror("Read parent");
-						exit(6);
+						perror("Read in app");
+						exit(8);
 					}
 
+					printf("Recibi de i=%d, n=%d : %s\n\n", i, read_val,ans);
 
-					printf("Recibi de i=%d, n=%d : %s\n\n", i, read_val,respuesta);
+					// TODO: escribir al buffer de anss
 
-
-					// TODO: escribir al buffer de respuestas
-
-
-					// ya que termino de procesar el archivo, le pasamos uno nuevo
-					// pero solo si quedan cosas para mandar
-					if(current_file_sent < num_files)
-						write(slaves[i].app_to_slave[WRITE], &(argv[current_file_sent]), sizeof(char *));
-						current_file_sent++;	
+                                        // Send new files if there are any left
+					if(curr_files_sent < num_files)
+						write(slaves[i].app_to_slave[WRITE], &(argv[curr_files_sent++]), sizeof(char *));
 				}
 			}
 
-			// restauramos los fds a los originales
+                        // Restore fds because select is destructive
 			read_fd = backup_read_fd;
 		}
 
 		printf("Cerrado el ciclo original\n");
 		
-		// una vez que procesamos todo, cerramos por completo los pipes
-		for(int j=0; j<num_slaves; j++){
-			close(slaves[j].app_to_slave[WRITE]);
-			close(slaves[j].slave_to_app[READ]);
+		// Finished processing so we close pipes
+		for(int i = 0; i < num_slaves; i++){
+                    if( close(slaves[i].app_to_slave[WRITE]) == -1 || close(slaves[i].slave_to_app[READ]) == -1) {
+                        perror("Closing pipes in app");
+                        exit(9);
+                    }
 
-			// TODO: matar al hijo pero de manera linda
-			kill(slaves[j].pid, SIGKILL);
+                    // TODO: matar al hijo pero de manera linda
+                    kill(slaves[i].pid, SIGKILL);
 		}
 		
 
 		printf("Matamos todos los hijos?\n");
 	}
+
+        return 0;
 }
