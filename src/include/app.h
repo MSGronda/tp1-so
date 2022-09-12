e6260fcce2a350c621510e296ff8fe2b2e7cf827
Manuel Dithurbide <mdithurbide@itba.edu.ar>
2022-09-12 12:24:16 -0300

    Added documentation for app and slave

--- src/include/app.h
+++ src/include/app.h
@@ -23,10 +23,45 @@
 #include <string.h>
 #include <semaphore.h>
 
-
-void redirect_fd(int oldfd, int newfd);
+/*
+ * << send_file >>
+ * ----------------------------------------------------------------------
+ * Description: Writes the * src recieved on the fd provided.
+ *              Checks for possible error during write().
+ * ----------------------------------------------------------------------
+ * Recieves: 
+ *      [fd] = file descriptor to write
+ *      [src] = Pointer to the string to be written
+ * Returns:--
+ */
 void send_file(int fd, char ** src);
+
+/*
+ * << write_to_shm >>
+ * ----------------------------------------------------------------------
+ * Description: Writes a [hash_data] struct the shm at a given offset. 
+ *              Signals the semaphore recieved after writing.
+ * ----------------------------------------------------------------------
+ * Recieves: 
+ *      [fd] = file descriptor of the shm
+ *      [addr] = semaphore address
+ *      [hash_data] = struct containing md5 hash info of a given file
+ *      [curr_files_shm] = current file count for calculating offset
+ * Returns:--
+ */
 void write_to_shm(int fd, sem_t * addr, hash_info * hash_data, int curr_files_shm); 
+
+/*
+ * << is_regular_file >>
+ * ----------------------------------------------------------------------
+ * Description: Checks is a provided path is a regular file
+ * ----------------------------------------------------------------------
+ * Recieves: 
+ *      [path] = path to a given file
+ * Returns:
+ *      1 in case it is a regular file
+ *      0 if not
+ */
 int is_regular_file(const char *path);
 
 
--- src/include/slave.h
+++ src/include/slave.h
@@ -13,6 +13,19 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 
+/*
+ * << slave >>
+ * ----------------------------------------------------------------------
+ * Description: Slave is connected via two pipes to a process. 
+ *              It calculates the md5 of a recieved file (app_to_slave)
+ *              Returns the result through the other pipe (slave_to_app)
+ * ----------------------------------------------------------------------
+ * Recieves: 
+ *      [app_to_slave] = Array containg the fd of the (App -> Slave) pipe
+ *      [slave_to_a] = Array containg the fd of the (App -> Slave) pipe
+ * Returns:
+ *      0 if no errors occur
+ */
 int slave(int * app_to_slave, int * slave_to_app);
 
 #endif
